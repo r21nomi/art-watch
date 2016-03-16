6749bf10034f963fa695c813b6cc2aca74602474
r21nomi <ryotakosu@gmail.com>
2016-03-16 10:14:16 +0900

    :heavy_plus_sign: Add splash and login page

---
+++ mobile/src/main/kotlin/com/nomi/artwatch/ui/activity/LoginActivity.kt
@@ -0,0 +1,66 @@
+package com.nomi.artwatch.ui.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import butterknife.bindView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.animation.GlideAnimation
+import com.bumptech.glide.request.target.SimpleTarget
+import com.nomi.artwatch.R
+import com.nomi.artwatch.di.component.ActivityComponent
+import com.nomi.artwatch.model.LoginModel
+import pl.droidsonroids.gif.GifImageView
+import rx.android.schedulers.AndroidSchedulers
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ * Created by Ryota Niinomi on 16/03/15.
+ */
+class LoginActivity : InjectActivity() {
+
+    @Inject
+    lateinit var mLoginModel: LoginModel
+
+    val mRootView : View by bindView(R.id.root)
+    val mGifView : GifImageView by bindView(R.id.gifView)
+    val mLoginText : TextView by bindView(R.id.loginText)
+
+    override fun injectDependency(component: ActivityComponent) {
+        component.inject(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+        Glide.with(this).load(R.raw.image_1).asGif().into(object : SimpleTarget<GifDrawable>() {
+            override fun onResourceReady(resource: GifDrawable, glideAnimation: GlideAnimation<in GifDrawable>) {
+                mGifView.setImageDrawable(resource)
+                resource.start()
+            }
+        })
+
+        mLoginText.setOnClickListener({
+            authorize()
+        })
+    }
+
+    private fun authorize() {
+        mLoginModel
+                .login()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ authUrl ->
+                    Timber.d("Auth URL : " + authUrl)
+                    startActivity(Intent("android.intent.action.VIEW", Uri.parse(authUrl)))
+
+                }, { throwable ->
+                    Timber.w(throwable, throwable.message)
+                })
+    }
+}
