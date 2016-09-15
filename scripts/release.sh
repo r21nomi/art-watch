1d407c3a4bdff42bbc370fe826cc74f24eb6dd2f
r21nomi <ryotakosu@gmail.com>
2016-09-15 19:27:01 +0900

    Move declaration of TAG_NAME

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install jq
 
-echo "TAG_NAME : " + $TAG_NAME
+echo "TAG_NAME :" $TAG_NAME
 
 UPLOAD_URL=`curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$CIRCLE_TAG | jq '.upload_url'`
 UPLOAD_URL=`echo $UPLOAD_URL | sed -e 's/{?name,label}/?name=/g' | sed -e 's/"//g'`
