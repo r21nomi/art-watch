e1d295dfe31a5cc14d7d04a5bcafd792ca1aab19
r21nomi <ryotakosu@gmail.com>
2016-09-15 12:49:01 +0900

    Fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,14 +4,12 @@ sudo apt-get install jq
 
 echo "start -------------------------------------------------------------------------"
 
-echo https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$CIRCLE_TAG
+echo "access token : " + $GITHUB_ACCESS_TOKEN
 
-UPLOAD_URL=`curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$CIRCLE_TAG | jq '.upload_url'`
+UPLOAD_URL='curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$CIRCLE_TAG | jq ".upload_url"'
 
-echo $UPLOAD_URL
+echo "URL : " + $UPLOAD_URL
 
-echo curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$CIRCLE_TAG | jq '.upload_url'
-
-UPLOAD_URL=`echo $UPLOAD_URL | sed -e 's/{?name,label}/?name=/g' | sed -e 's/"//g'`
+UPLOAD_URL=echo $UPLOAD_URL | sed -e 's/{?name,label}/?name=/g' | sed -e 's/"//g'
 
 curl --data-binary @$1 -H "Content-Type: application/zip" -H "Authorization: token $GITHUB_ACCESS_TOKEN" $UPLOAD_URL$2
